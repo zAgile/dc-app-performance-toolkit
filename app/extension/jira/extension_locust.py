172d6375bbc1965b0ae3657796d9731a55ecc7d5
daniacahuana <dcahuana@zagile.com>
2022-05-23 19:14:16 -0500

    jmeter and locust

--- app/extension/jira/extension_locust.py
+++ app/extension/jira/extension_locust.py
@@ -2,8 +2,8 @@ import re
 from locustio.common_utils import init_logger, jira_measure, run_as_specific_user  # noqa F401
 
 logger = init_logger(app_type='jira')
-issue_key="PROJT-83"
-case_id="500J000000VTOJVIA5"
+issue_key="PROJT-1"
+case_id="500J000000VUTqB"
 
 @jira_measure("locust_app_specific_action")
 # @run_as_specific_user(username='admin', password='admin')  # run as specific user
@@ -56,6 +56,22 @@ def app_specific_action(locust):
     assert 'feeds' in content
 
 
+    """GET FEEDS - SHOW MORE"""
+    r = locust.get(f'/rest/zagile-sf/1.0/{issue_key}/feed/500J000000VUTqBIAX?pageSize=20',
+                   catch_response=True)
+    content = r.content.decode('utf-8')
+
+    token_pattern_example = '"token":"(.+?)"'
+    id_pattern_example = '"id":"(.+?)"'
+    token = re.findall(token_pattern_example, content)
+    id = re.findall(id_pattern_example, content)
+
+    logger.locust_info(f'token: {token}, id: {id}')
+    if 'feeds' not in content:
+        logger.error(f"'feeds do not exist in {content}")
+    assert 'feeds' in content
+
+
     """GET EMAILS"""
     r = locust.get(f'/rest/zagile-sf/1.0/{issue_key}/email/{case_id}?limit=20&offset=0',
                    catch_response=True)
@@ -76,19 +92,19 @@ def app_specific_action(locust):
 
 
     """POST IMPORT"""
-    pyload = {"entityId": "500J000000VTNhkIAH", "filename": "image (1).png", "contentType": "image/png", "attachmentObjName": "Attachment", "attachmentId": "00PJ0000009enmLMAQ"}
+    pyload = {"entityId":"500J000000VUTqBIAX","filename":"case1.jpg","contentType":"JPG","attachmentObjName":"ContentVersion","attachmentId":"068J0000001j1diIAA"}
     r = locust.post(
-        f'/rest/zagile-sf/1.0/attachment/{issue_key}/import?token=Zr%2BosnUZUJuURjpRsEkTUhq2%2B4ycoI8546O1sNlhWmc%3D',
+        f'/rest/zagile-sf/1.0/attachment/{issue_key}/import?token=fXjVgSWcM%2FtAbfa6RqaTTn4OCsrJyt2dlTWYCR1spss%3D',
         json=pyload, headers={'content-type': 'application/json', 'Accept': 'application/json', 'Content-Length': '155'},
         catch_response=True)  # call app-specific POST endpoint
 
-    assert r.status_code is 200, "import string after successful POST request' was not found "  # assertion after POST request
+    assert r.status_code is 200, "import string was not found after request POST "  # assertion after POST request
 
 
     """POST DOWNLOAD"""
     r = locust.post(
-        f'/plugins/servlet/downloadSalesforceAttachment?attachmentId=00PJ0000009eqg9MAA&issueKey={issue_key}&entityId=500J000000VTNhkIAH&attachmentName=bar.jpg&token=8u3Bgg%2FyBFtHMt2uFFO4Fw5s9QEtP%2BGZWymYM2M2qDM%3D&sObjName=Attachment&contentType=image/jpeg',
+        f'https://jiracluster.dc.zdevbox.net/plugins/servlet/downloadSalesforceAttachment?attachmentId=068J0000001j1diIAA&issueKey={issue_key}&entityId=500J000000VUTqBIAX&attachmentName=case1.jpg&token=fXjVgSWcM%2FtAbfa6RqaTTn4OCsrJyt2dlTWYCR1spss%3D&sObjName=ContentVersion&contentType=JPG',
         headers={'content-type': 'application/json', 'Accept': 'application/json', 'X-Atlassian-Token': 'no-check'},
         catch_response=True)
 
-    assert r.status_code is 200, "download string after successful POST request' was not found "
+    assert r.status_code is 200, "download string was not found after request POST"
--- app/extension/jira/extension_ui.py
+++ app/extension/jira/extension_ui.py
@@ -7,30 +7,10 @@ from selenium_ui.conftest import print_timing
 from selenium_ui.jira.pages.pages import Login
 from util.conf import JIRA_SETTINGS
 
-
 def app_specific_action(webdriver, datasets):
     page = BasePage(webdriver)
     if datasets['custom_issues']:
         issue_key = datasets['custom_issue_key']
-
-    # To run action as specific user uncomment code bellow.
-    # NOTE: If app_specific_action is running as specific user, make sure that app_specific_action is running
-    # just before test_2_selenium_z_log_out action
-    #
-    # @print_timing("selenium_app_specific_user_login")
-    # def measure():
-    #     def app_specific_user_login(username='admin', password='admin'):
-    #         login_page = Login(webdriver)
-    #         login_page.delete_all_cookies()
-    #         login_page.go_to()
-    #         login_page.set_credentials(username=username, password=password)
-    #         if login_page.is_first_login():
-    #             login_page.first_login_setup()
-    #         if login_page.is_first_login_second_page():
-    #             login_page.first_login_second_page_setup()
-    #         login_page.wait_for_page_loaded()
-    #     app_specific_user_login(username='admin', password='admin')
-    # measure()
 
     @print_timing("selenium_app_custom_action")
     def measure():
@@ -38,7 +18,26 @@ def app_specific_action(webdriver, datasets):
         def sub_measure():
             page.go_to_url(f"{JIRA_SETTINGS.server_url}/browse/{issue_key}")
             page.wait_until_visible((By.ID, "summary-val"))  # Wait for summary field visible
-            page.wait_until_visible((By.XPATH, '//*[@id="container-wrap"]/table[contains(@class,"sf-properties-panel")]'))  # Wait for you app-specific UI element by ID selector
+            page.wait_until_visible((By.XPATH, '//*[@id="container-wrap"]/table/tbody/tr/td[1]/a'))  # Wait for you app-specific UI element by ID selector
+        sub_measure()
+
+        @print_timing("selenium_app_custom_action:open_detaill")
+        def sub_measure():
+            page.go_to_url(f"{JIRA_SETTINGS.server_url}/browse/{issue_key}")
+            page.wait_until_visible((By.ID, "summary-val"))
+            locator = (By.XPATH, "//*[@id='container-wrap']/table/tbody/tr/td/a[contains(@onclick,'SalesforcePropertiesDetails')]")
+            table = page.wait_until_visible(locator)
+            element = table.get_attribute("onclick")
+            #if "SalesforcePropertiesDetails" in element:
+            d = element[45:-2]
+            page.go_to_url(f"{JIRA_SETTINGS.server_url}"+d)
+            page.wait_until_visible((By.XPATH, '//*[@id="details"]/div/div[1]/h2'))
+            page.wait_until_visible((By.XPATH, '//*[@id="commentsTab"]')).click()
+            page.wait_until_visible((By.XPATH, '//*[@id="attachmentsTab"]')).click()
+            page.wait_until_visible((By.XPATH, '//*[@id="feedsTab"]')).click()
+            page.wait_until_visible((By.XPATH, '//*[@id="emailsTab"]')).click()
+
         sub_measure()
     measure()
+
 
